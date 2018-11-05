018a4109eeaca8599b6b2691959ad692b3462755
Gad Maor <gadm@il.ibm.com>
2018-11-05 02:31:07 +0200

    Adds item input loops and methods

--- CashRegister/src/CashRegisterDriver.java
+++ CashRegister/src/CashRegisterDriver.java
@@ -13,9 +13,6 @@ public class CashRegisterDriver {
                 new Item("Perfume", 80), new Item("Jeans", 75),
                 new Item("Sport shoes", 150), new Item("Kitchen table", 250)
         };
-        for (Item item : inventory) {
-            System.out.println(item);
-        }
         // create a Scanner to obtain input from the command window
         Scanner input = new Scanner(System.in);
         // Declare a new CashRegister pointer
@@ -35,10 +32,18 @@ public class CashRegisterDriver {
                 cr = new CashRegister();
                 break;
         }
-        System.out.printf("Cash register initialized with %.2f$.\n", cr.getTotalSum());
-        // Main Cash Register function - get shoppingList, payment and move tp next customer
+        System.out.printf("Cash register initialized with %.2f$.%n%n", cr.getTotalSum());
+        // Main Cash Register function - get shoppingList, payment and continue with next customer
         while (true) {
-         //   displayStoreMenu();
+            // Get items until customer inputs -1
+            // printStoreMenu();
+            do {
+                selection = getItemSelection(inventory, input);
+                if (selection == -1 )
+                    continue;
+                int amount = getAmount(inventory[selection].getName(), input);
+                System.out.println("You've chosen to purchase: " + amount + " " + inventory[selection].getName() + "s.\n");
+            } while (selection != -1);
             break;
         }
         // getShoppingList(shoppingList);
@@ -57,11 +62,19 @@ public class CashRegisterDriver {
         System.out.println("You paid: " + payment + "$\n" + "Your change is: " + cr.getPayment(payment,shoppingList) + "$");
         System.out.printf("Cash Register now has %.2f$ left.", cr.getTotalSum());*/
     }
+
+    /** Prints a welcome message for the store
+     *
+     */
     public static void printWelcomeScreen() {
         System.out.println("+--------------------------------+\n" +
                             "+Hello and welcome to OpenU-Mart!+\n" +
                             "+--------------------------------+");
     }
+
+    /** Prints the CashRegister initialization request
+     *
+     */
     public static void printCashRegisterInitializationMessage(){
         System.out.println("You must first initialize the Cash Register service.\n" +
                            "Please choose one of the following options:\n" +
@@ -71,5 +84,18 @@ public class CashRegisterDriver {
 
         //System.out.println("Please select the requested operation from the following menu(type numeric selection): \n");
 
+    }
+    public static int getItemSelection(Item[] inventory, Scanner input) {
+        System.out.println("Please select which item you'd like to purchase from the following list.\n" +
+                           "Enter -1 to finish selection and go to payment.\n");
+        for (int counter = 0; counter < inventory.length; ++counter) {
+            System.out.println(counter + ")" + inventory[counter]);
+        }
+        System.out.print("Enter selection: ");
+        return input.nextInt();
+    }
+    public static int getAmount(String itemName, Scanner input) {
+        System.out.printf("Please enter the amount of %ss you'd like to purchase: ", itemName);
+        return input.nextInt();
     }
 }
